53ae691852018c500d6d27cbb89b0f5673b17070
Edward Mizuka <mizuka.wu@outlook.com>
2020-03-23 18:05:51 +0800

    Exclude refs created by GitHub for pull request
    Prevents "deny updating a hidden ref" error when pushing GitHub pull requests to the mirror.
    
    Co-authored-by: Dominik Schilling <dominikschilling+git@gmail.com>

--- git-mirror.sh
+++ git-mirror.sh
@@ -13,4 +13,6 @@ echo "DESTINATION=$DESTINATION_REPO"
 git clone --mirror "$SOURCE_REPO" && cd `basename "$SOURCE_REPO"`
 git remote set-url --push origin "$DESTINATION_REPO"
 git fetch -p origin
+# Exclude refs created by GitHub for pull request.
+git for-each-ref --format 'delete %(refname)' refs/pull | git update-ref --stdin
 git push --mirror
