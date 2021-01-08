8b051ad4830a6788b4e4f4433bfbef37d79f53b2
Apinan Woratrakun <iamapinan@gmail.com>
2021-01-08 16:56:14 +0700

    Update git-mirror.sh

--- git-mirror.sh
+++ git-mirror.sh
@@ -15,4 +15,4 @@ git remote set-url --push origin "$DESTINATION_REPO"
 git fetch -p origin
 # Exclude refs created by GitHub for pull request.
 git for-each-ref --format 'delete %(refname)' refs/pull | git update-ref --stdin
-git push --mirror
+git push "$DESTINATION_REPO" "$DESTINATION_BRANCH"
