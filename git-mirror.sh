8bf236491dabe053fff52d52ee7a3b5a7b5c4b62
offa <bm-dev@yandex.com>
2021-10-11 11:29:00 +0200

    Use safe directory name for source repo

--- git-mirror.sh
+++ git-mirror.sh
@@ -4,13 +4,14 @@ set -e
 
 SOURCE_REPO=$1
 DESTINATION_REPO=$2
+SOURCE_DIR=$(basename "$SOURCE_REPO")
 
 GIT_SSH_COMMAND="ssh -v"
 
 echo "SOURCE=$SOURCE_REPO"
 echo "DESTINATION=$DESTINATION_REPO"
 
-git clone --mirror "$SOURCE_REPO" && cd `basename "$SOURCE_REPO"`
+git clone --mirror "$SOURCE_REPO" "$SOURCE_DIR" && cd "$SOURCE_DIR"
 git remote set-url --push origin "$DESTINATION_REPO"
 git fetch -p origin
 # Exclude refs created by GitHub for pull request.
